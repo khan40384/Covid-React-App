600e6ff0938a4d6855f82bc5bcc32d23d26885d2
Atif Khan <atif8486@gmail.com>
2020-07-01 00:35:24 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import {Cards, Charts, CountryPicker} from './components';
+import styles from './App-modules.css';
+import {fetchData } from "./api";
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+async componentDidMount(){
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData});
+  
+}
+
+handleCountryChange = async (country) => {
+  let fetchedData ;
+  if(country ==='global'){
+     fetchedData = await fetchData();
+  }else{
+     fetchedData = await fetchData(country);
+    }
+  
+  console.log(country);
+  this.setState( { data: fetchedData, country:country });
+  
+  
+
+}
+    render(){
+      console.log(this.state);
+      const { data,country } = this.state;
+       return (
+           <div className={styles.container}>
+             <Cards data={data}/>
+             <CountryPicker handleCountryChange={this.handleCountryChange}/>
+             <Charts data={data} country ={country}/>
+           </div>
+       )
+    }
+}
+export default App;
