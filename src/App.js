573f7c49bd8b3adb5b2e240ad6948a6cd1bb834e
Atif Khan <atif8486@gmail.com>
2020-07-12 21:30:12 +0530

    add faetures

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import {Cards, Charts, CountryPicker} from './components';
+import Button from '@material-ui/core/Button';
 import styles from './App-modules.css';
 import {fetchData } from "./api";
 import image from "./image/image.png"
@@ -17,8 +17,18 @@ async componentDidMount(){
     this.setState({data: fetchedData});
   
 }
+Exam= async()=>{
+  fetch('http://localhost:4000/run')
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
 
 handleCountryChange = async (country) => {
+  
   let fetchedData ;
   if(country ==='global'){
      fetchedData = await fetchData();
@@ -36,12 +46,23 @@ handleCountryChange = async (country) => {
       console.log(this.state);
       const { data,country } = this.state;
        return (
+         
            <div className={styles.container}>
-             <img src = {image} alt ="COVID-19" style ={{max_width:"770px",width:"100%",marginTop:"0",marginBottom:"10%" }}/>
+             <div style={{display:'flex',marginBottom:'2%', alignItems :'center', justifyContent :'center'}}>
+             <div style={{flex:'1'}}>
+             <img src = {image} alt ="COVID-19" style ={{max_width:"370px",width:"100%",marginTop:"0"}}/>
+             </div>
+             <div style={{flex:'3'}}>
+             <Button onClick={()=>this.Exam()} variant="contained" color="primary" style={{float:'right'}}>
+              Need Help!
+             </Button>
+             </div>
+             </div>
              <Cards data={data}/>
              <CountryPicker handleCountryChange={this.handleCountryChange}/>
              <Charts data={data} country ={country}/>
            </div>
+           
        )
     }
 }
